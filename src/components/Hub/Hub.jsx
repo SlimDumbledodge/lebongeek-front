9cc204c9df39e580d1d2c47f42c0fe2855ba78c2
Amael Rosales <amael.rosales@gmail.com>
2023-12-02 23:40:20 +0100

    DONE - V1 : Re découpage en plusieurs petit composants pour le hub ainsi qu'ajout des routes permettant de se rendre sur l'url /hub avec react router dom, le design pourrait être améliorer mais c'est une base cohérente

--- src/components/Hub/Hub.jsx
+++ src/components/Hub/Hub.jsx
@@ -1,87 +1,14 @@
+import Banner from './Banner/Banner';
+import Description from './Description/Description';
+import Inventory from './Inventory/Inventory';
 import './Hub.scss';
 
 const Hub = () => {
   return (
     <>
-      <img
-        src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-        alt=""
-        className="banner"
-      />
-      <section className="description__container">
-        <div className="avatar__pseudo">
-          <h2 className="pseudo">PSEUDO</h2>
-          <img
-            src="https://cdn.icon-icons.com/icons2/2859/PNG/512/avatar_face_girl_female_woman_profile_smiley_happy_people_icon_181662.png"
-            alt=""
-            className="avatar"
-          />
-        </div>
-        <p className="description__text">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem cum
-          tempora incidunt eum consectet
-        </p>
-      </section>
-      <section className="inventory">
-        <h2 className="inventory__title">INVENTAIRE</h2>
-        <div className="grid__wrapper">
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-          <img
-            src="https://www.freewebheaders.com/wp-content/gallery/abstract-size-800x200/red-blue-yellow-smoke-abstract-header-800x200.jpg"
-            alt=""
-            className="inventory__item"
-          />
-        </div>
-      </section>
+      <Banner />
+      <Description />
+      <Inventory />
     </>
   );
 };
