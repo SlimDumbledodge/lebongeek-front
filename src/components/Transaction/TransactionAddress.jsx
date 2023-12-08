be069a715071343320fb56476c1f5de0ab29f979
Etudiant O'clock <you@example.com>
2023-12-08 11:35:36 +0100

    WIP transaction

--- src/components/Transaction/TransactionAddress.jsx
+++ src/components/Transaction/TransactionAddress.jsx
@@ -4,60 +4,64 @@ const TransactionAddress = () => {
   return (
     <div className="transaction__address__container">
       <div className="transaction__address__input__container">
-        <label htmlFor="transaction__country__label">Pays</label>
-        <select name="countrySelect" id="transaction__country__select">
-          <option className="transaction__country__choice" value="">
+        <label className="transaction__label" htmlFor="countryLabel">
+          Pays
+        </label>
+        <select name="countrySelect" className="transaction__select">
+          <option className="transaction__choice" value="">
             Choisissez...
           </option>
-          <option className="transaction__country__choice" value="france">
+          <option className="transaction__choice" value="france">
             France
           </option>
         </select>
       </div>
 
       <div className="transaction__address__input__container">
-        <label htmlFor="transaction__street__label">Voie</label>
-        <select name="streetSelect" id="transaction__street__select">
-          <option className="transaction__street__choice" value="">
+        <label className="transaction__label" htmlFor="streetLabel">
+          Voie
+        </label>
+        <select name="streetSelect" className="transaction__select">
+          <option className="transaction__choice" value="">
             Choisissez...
           </option>
-          <option className="transaction__street__choice" value="rue">
+          <option className="transaction__choice" value="rue">
             Rue
           </option>
         </select>
       </div>
 
       <div className="transaction__address__input__container">
-        <label htmlFor="transaction__postalcode__label">Code postal</label>
-        <input
-          className="transaction__postalcode__input"
-          type="text"
-          placeholder="34000"
-        />
+        <label className="transaction__label" htmlFor="postalCodeLabel">
+          Code postal
+        </label>
+        <input className="transaction__input" type="text" placeholder="34000" />
       </div>
 
       <div className="transaction__address__input__container">
-        <label htmlFor="transaction__streetnumber__label">N° de voie</label>
-        <input
-          className="transaction__streetnumber__input"
-          type="text"
-          placeholder="85"
-        />
+        <label className="transaction__label" htmlFor="streetNumberLabel">
+          N° de voie
+        </label>
+        <input className="transaction__input" type="text" placeholder="85" />
       </div>
 
       <div className="transaction__address__input__container">
-        <label htmlFor="transaction__streetname__label">Nom de voie</label>
+        <label className="transaction__label" htmlFor="numberStreetLabel">
+          Nom de voie
+        </label>
         <input
-          className="transaction__streetname__input"
+          className="transaction__input"
           type="text"
           placeholder="Saint-Exupéry"
         />
       </div>
 
       <div className="transaction__address__input__container">
-        <label htmlFor="transaction__city__label">Ville</label>
+        <label className="transaction__label" htmlFor="cityLabel">
+          Ville
+        </label>
         <input
-          className="transaction__city__input"
+          className="transaction__input"
           type="text"
           placeholder="Montpellier"
         />
