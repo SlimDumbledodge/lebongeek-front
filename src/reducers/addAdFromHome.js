0517e22773520e4b0dafa31b740441beb3922797
Amael Rosales <amael.rosales@oclock.school>
2023-12-11 10:02:29 +0100

    Préparation de la requête pour envoyer une annonce et un produit depuis la nav

---
+++ src/reducers/addAdFromHome.js
@@ -0,0 +1,79 @@
+import {
+  CHANGE_ADD_AD_FROM_HOME_CATEGORY,
+  CHANGE_ADD_AD_FROM_HOME_DESCRIPTION,
+  CHANGE_ADD_AD_FROM_HOME_STATE,
+  CHANGE_ADD_AD_FROM_HOME_PRODUCT_TITLE,
+  CHANGE_ADD_AD_FROM_HOME_PRICE,
+  CHANGE_ADD_AD_FROM_HOME_LOCATION,
+  CHANGE_ADD_AD_FROM_HOME_PRODUCT_YEAR,
+  CHANGE_ADD_AD_FROM_HOME_PRODUCT_SERIAL_NUMBER,
+} from '../actions/addAdFromHome';
+
+const initialState = {
+  addAdFromHomeProductTitle: '',
+  addAdFromHomeProductPhoto: '../src/assets/images/boutique.jpg',
+  addAdFromHomeProductYear: '',
+  addAdFromHomeProductSerialNumber: '',
+
+  addAdFromHomeDescription: '',
+  addAdFromHomePrice: '',
+  addAdFromHomeState: '',
+  addAdFromHomeLocation: '',
+  addAdFromHomeCategory: '',
+};
+
+const addAdFromHomeReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_ADD_AD_FROM_HOME_PRODUCT_TITLE:
+      return {
+        ...state,
+        addAdFromHomeProductTitle: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_DESCRIPTION:
+      return {
+        ...state,
+        addAdFromHomeDescription: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_STATE:
+      return {
+        ...state,
+        addAdFromHomeState: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_CATEGORY:
+      return {
+        ...state,
+        addAdFromHomeCategory: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_PRICE:
+      return {
+        ...state,
+        addAdFromHomePrice: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_LOCATION:
+      return {
+        ...state,
+        addAdFromHomeLocation: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_PRODUCT_YEAR:
+      return {
+        ...state,
+        addAdFromHomeProductYear: action.newValue,
+      };
+
+    case CHANGE_ADD_AD_FROM_HOME_PRODUCT_SERIAL_NUMBER:
+      return {
+        ...state,
+        addAdFromHomeProductSerialNumber: action.newValue,
+      };
+    default:
+      return state;
+  }
+};
+
+export default addAdFromHomeReducer;
