fa5e10e2d7f9f479b042eb9a8785448bc69ff5f8
Leila Amin <leila.amin@oclock.school>
2023-12-21 10:40:55 +0100

    encore en cours ++

--- src/components/Hub/Banner/Banner.jsx
+++ src/components/Hub/Banner/Banner.jsx
@@ -1,7 +1,9 @@
 import './Banner.scss';
 
 const Banner = () => {
-  return <img src="src/assets/images/guy.jpg" alt="" className="hub__banner" />;
+  return (
+    <img src="src/assets/images/bbhub.png" alt="" className="hub__banner" />
+  );
 };
 
 export default Banner;
